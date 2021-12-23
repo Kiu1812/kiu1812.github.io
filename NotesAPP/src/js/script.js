19cefe0a1ffb120b7ba9a69993e5bb01cb9228df
Kiu1812 <44612925+Kiu1812@users.noreply.github.com>
2021-12-23 22:54:23 +0100

    Update

--- NotesAPP/src/js/script.js
+++ NotesAPP/src/js/script.js
@@ -1,5 +1,4 @@
 var editModeActive = false;
-var resizing = false;
 var removeActive = false;
 
 function editMode() {
@@ -9,7 +8,7 @@ function editMode() {
   let post_content = document.getElementsByClassName("post_content");
   let buttons = document.getElementsByClassName("bt_edit");
   let editButton = document.getElementById("bt_editMode");
-  if (!removeActive) {
+  if (!removeActive && post.length >= 1) {
     if (editModeActive) {
       editModeActive = false;
       for (let i = 0; i < buttons.length; i++) {
@@ -128,10 +127,30 @@ function disableEdit(i) {
 function enableOptions(id) {
   id = id-1;
   let options = document.getElementsByClassName("noteOptions");
-  if (options[id].classList.contains("no-visible"))
+  if (options[id].classList.contains("no-visible")) {
     options[id].classList.remove("no-visible");
+    checkColor(id);
+  }
   else
     options[id].classList.add("no-visible");
+}
+
+function checkColor(id) {
+  let post = document.getElementsByClassName("post");
+  let post_head = document.getElementsByClassName("post_head");
+  let colorInput = document.getElementsByClassName("colorInput");
+  let leftBT = document.getElementsByClassName("colorSelectLeftBT");
+
+  if (leftBT[id].id == "colorSelectLeftBT1") {
+    let hexa = getHEX(post[id].style.backgroundColor);
+    colorInput[id].value = hexa;
+  } else if (leftBT[id].id == "colorSelectLeftBT2") {
+    let hexa = getHEX(post_head[id].style.backgroundColor);
+    colorInput[id].value = hexa;
+  } else {
+    let hexa = getHEX(post_head[id].style.color);
+    colorInput[id].value = hexa;
+  }
 }
 
 function changeBack(id) {
@@ -140,10 +159,146 @@ function changeBack(id) {
   post[id-1].style.backgroundColor = colorInput[id-1].value;
 }
 
+function changeTextBack(id) {
+  let post_head = document.getElementsByClassName("post_head");
+  let post_header = document.getElementsByClassName("post_header");
+  let post_content = document.getElementsByClassName("post_content");
+  let post_text = document.getElementsByClassName("post_text");
+  let colorInput = document.getElementsByClassName("colorInput");
+
+  post_head[id-1].style.backgroundColor = colorInput[id-1].value;
+  post_header[id-1].style.backgroundColor = colorInput[id-1].value;
+  post_content[id-1].style.backgroundColor = colorInput[id-1].value;
+  post_text[id-1].style.backgroundColor = colorInput[id-1].value;
+}
+
+function changeTextColor(id) {
+  let post_head = document.getElementsByClassName("post_head");
+  let post_header = document.getElementsByClassName("post_header");
+  let post_content = document.getElementsByClassName("post_content");
+  let post_text = document.getElementsByClassName("post_text");
+  let colorInput = document.getElementsByClassName("colorInput");
+
+  post_head[id-1].style.color = colorInput[id-1].value;
+  post_header[id-1].style.color = colorInput[id-1].value;
+  post_content[id-1].style.color = colorInput[id-1].value;
+  post_text[id-1].style.color = colorInput[id-1].value;
+}
+
+function selectColorToRight(id) {
+  id = id-1;
+  let colorSelectShow = document.getElementsByClassName("colorSelectShow");
+  let leftBT = document.getElementsByClassName("colorSelectLeftBT");
+  let rightBT = document.getElementsByClassName("colorSelectRightBT");
+  let colorInput = document.getElementsByClassName("colorInput");
+  
+  for (let i = 1; i <= 2; i++) {
+    if (colorSelectShow[id].classList.contains("colorSelectShow"+i)) {
+      colorSelectShow[id].classList.remove("colorSelectShow"+i);
+      colorSelectShow[id].classList.add("colorSelectShow"+(i+1));
+
+      switch (i+1) {
+        case 2: {
+          colorSelectShow[id].innerHTML = "Text Background";
+          colorInput[id].setAttribute("oninput", "changeTextBack("+(id+1)+")");
+          let post_head = document.getElementsByClassName("post_head");
+          let hexa = getHEX(post_head[id].style.backgroundColor);
+          colorInput[id].value = hexa;
+          break;
+        }
+        case 3: {
+          colorSelectShow[id].innerHTML = "Text Color";
+          colorInput[id].setAttribute("oninput", "changeTextColor("+(id+1)+")");
+          let post_head = document.getElementsByClassName("post_head");
+          let hexa = getHEX(post_head[id].style.color);
+          colorInput[id].value = hexa;
+          break;
+        }
+      }
+
+      leftBT[id].setAttribute("id","colorSelectLeftBT"+(i+1));
+      rightBT[id].setAttribute("id","colorSelectRightBT"+(i+1));
+      break;
+    }
+  }
+}
+
+function selectColorToLeft(id) {
+  id = id-1;
+  let colorSelectShow = document.getElementsByClassName("colorSelectShow");
+  let leftBT = document.getElementsByClassName("colorSelectLeftBT");
+  let rightBT = document.getElementsByClassName("colorSelectRightBT");
+  let colorInput = document.getElementsByClassName("colorInput");
+
+  for (let i = 2; i <= 3; i++) {
+    if (colorSelectShow[id].classList.contains("colorSelectShow"+i)) {
+      colorSelectShow[id].classList.remove("colorSelectShow"+i);
+      colorSelectShow[id].classList.add("colorSelectShow"+(i-1));
+
+      switch (i-1) {
+        case 1: {
+          colorSelectShow[id].innerHTML = "Post Background";
+          colorInput[id].setAttribute("oninput", "changeBack("+(id+1)+")");
+          let post = document.getElementsByClassName("post");
+          let hexa = getHEX(post[id].style.backgroundColor);
+          colorInput[id].value = hexa;
+          delete post;
+          break;
+        }
+        case 2: {
+          colorSelectShow[id].innerHTML = "Text Background";
+          colorInput[id].setAttribute("oninput", "changeTextBack("+(id+1)+")")
+          let post_head = document.getElementsByClassName("post_head");
+          let hexa = getHEX(post_head[id].style.backgroundColor);
+          colorInput[id].value = hexa;
+          break;
+        }
+      }
+
+      leftBT[id].setAttribute("id","colorSelectLeftBT"+(i-1));
+      rightBT[id].setAttribute("id","colorSelectRightBT"+(i-1));
+      break;
+    }
+  }
+}
+/*
+function HEXtoRGB(hex) {
+  var red = parseInt(hex[1]+hex[2],16);
+  var green = parseInt(hex[3]+hex[4],16);
+  var blue = parseInt(hex[5]+hex[6],16);
+}*/
+
+function componentToHex(c) {
+  var hex = c.toString(16);
+  return hex.length == 1 ? "0" + hex : hex;
+}
+
+function rgbToHex(r, g, b) {
+  return "#" + componentToHex(r) + componentToHex(g) + componentToHex(b);
+}
+
+function getRGBValues(str) {
+  var vals = str.substring(str.indexOf('(') +1, str.length -1).split(', ');
+  return {
+    'r': vals[0],
+    'g': vals[1],
+    'b': vals[2]
+  };
+}
+
+function getHEX(str) {
+  let rgb_values = getRGBValues(str);
+  rgb_values["r"] = parseInt(rgb_values["r"]);
+  rgb_values["g"] = parseInt(rgb_values["g"]);
+  rgb_values["b"] = parseInt(rgb_values["b"]);
+  let hexa = rgbToHex(rgb_values["r"], rgb_values["g"], rgb_values["b"]);
+  return hexa
+}
+
 function removePost() {
   var buttons = document.getElementsByClassName("bt_edit");
   var button = document.getElementById("bt_bin");
-  if (!editModeActive) {
+  if (!editModeActive && buttons.length >= 1 || removeActive) {
     if (removeActive) {
       removeActive = false;
       for (let i = 1; i <= buttons.length; i++) {
@@ -195,6 +350,8 @@ function removeSelf(cont) {
   let post = document.getElementsByClassName("post");
   post[cont].remove();
   var buttons = document.getElementsByClassName("bt_edit");
+  if (post.length == 0)
+    removePost()
   for (let i = 1; i <= buttons.length; i++) {
     buttons[i-1].setAttribute("onclick", "removeSelf("+(i-1)+")");
   } 
@@ -320,6 +477,7 @@ function setSizeClass(checkClass, nextClass, post, bt_addHeight, bt_addWidth, id
   checkSize(id+1)
 }
 
+
 // ADD ALL THE ONCLICK EVENTS TO POSTS
 function addEvents() {
   let post = document.getElementsByClassName("post");
@@ -329,6 +487,8 @@ function addEvents() {
   let bt_removeWidth = document.getElementsByClassName("bt_removeWidth");
   let bt_removeHeight = document.getElementsByClassName("bt_removeHeight");
   let colorInput = document.getElementsByClassName("colorInput");
+  let leftBT = document.getElementsByClassName("colorSelectLeftBT");
+  let rightBT = document.getElementsByClassName("colorSelectRightBT");
 
   for (let i = 1; i <= post.length; i++){
     post[i-1].removeAttribute("onclick");
@@ -339,6 +499,8 @@ function addEvents() {
     bt_removeWidth[i-1].removeAttribute("onclick");
     bt_removeHeight[i-1].removeAttribute("onclick");
     colorInput[i-1].removeAttribute("oninput");
+    leftBT[i-1].removeAttribute("onclick");
+    rightBT[i-1].removeAttribute("onclick");
   }
 
   for (let i = 1; i <= post.length; i++){
@@ -350,11 +512,27 @@ function addEvents() {
     bt_removeWidth[i-1].setAttribute("onclick","removeWidth("+i+")");
     bt_removeHeight[i-1].setAttribute("onclick","removeHeight("+i+")");
     colorInput[i-1].setAttribute("oninput", "changeBack("+i+")");
+    leftBT[i-1].setAttribute("onclick", "selectColorToLeft("+i+")");
+    rightBT[i-1].setAttribute("onclick", "selectColorToRight("+i+")");
   }
 }
 
 
 // CREATE POST
+function createMultiplePost() {
+  let amount = prompt("How many posts do you want? (default 1)");
+  if (amount == "") {
+    amount = 1;
+  }
+  if (!isNaN(amount)) {
+    for (let i = 0; i < amount; i++) {
+      createPost();
+    }
+  } else {
+    alert("Must be a valid number!");
+  }
+}
+
 function createPost() {
   if (!removeActive && !editModeActive) {
     let posts = document.getElementById("posts");
@@ -366,6 +544,7 @@ function createPost() {
     /* Post header*/
     let post_head = document.createElement("div");
     post_head.setAttribute("class", "post_head");
+    
     let post_header = document.createElement("h2");
     post_header.innerHTML = "Texto Aqui";
     post_header.setAttribute("class", "post_header");
@@ -380,12 +559,41 @@ function createPost() {
     noteOptions.classList.add("no-visible");
     let changeColorDiv = document.createElement("div");
     changeColorDiv.classList.add("noteSubOptions");
+    changeColorDiv.classList.add("sub2");
     changeColorDiv.classList.add("changeColorDiv");
     let colorInput = document.createElement("input");
     colorInput.setAttribute("type", "color");
     colorInput.classList.add("colorInput");
     colorInput.value = "#ffffff";
+    let colorSelectShow = document.createElement("p");
+    colorSelectShow.classList.add("colorSelectShow");
+    colorSelectShow.classList.add("colorSelectShow1");
+    
+    colorSelectShow.innerHTML = "Post Background";
+    //colorSelectShow.innerHTML = "Text Background";
+    //colorSelectShow.innerHTML = "Text Color";
+    let colorSelectLeftBT = document.createElement("button");
+    colorSelectLeftBT.setAttribute("id", "colorSelectLeftBT1");
+    colorSelectLeftBT.classList.add("colorSelectBT");
+    colorSelectLeftBT.classList.add("colorSelectLeftBT");
+    let leftImg = document.createElement("img");
+    leftImg.setAttribute("src", "src/img/left-right.png");
+    leftImg.setAttribute("width", "20px");
+    colorSelectLeftBT.appendChild(leftImg);
+    let colorSelectRightBT = document.createElement("button");
+    colorSelectRightBT.setAttribute("id", "colorSelectRightBT1");
+    colorSelectRightBT.classList.add("colorSelectBT");
+    colorSelectRightBT.classList.add("colorSelectRightBT");
+    let rightImg = document.createElement("img");
+    rightImg.setAttribute("src", "src/img/left-right.png");
+    rightImg.setAttribute("width", "20px");
+    colorSelectRightBT.appendChild(rightImg);
+
+
     changeColorDiv.appendChild(colorInput);
+    changeColorDiv.appendChild(colorSelectShow);
+    changeColorDiv.appendChild(colorSelectLeftBT);
+    changeColorDiv.appendChild(colorSelectRightBT);
     noteOptions.appendChild(changeColorDiv);
     let changeGroupDiv = document.createElement("div");
     changeGroupDiv.classList.add("noteSubOptions");
@@ -409,6 +617,7 @@ function createPost() {
     pre.innerHTML = "text example";
     post_content.appendChild(pre);
     post.appendChild(post_content);
+
 
     /* ChangeSize Div*/
     let addSize = document.createElement("div");
